53205b88dc5e8d771c3eaad2b0e8b3fb4b3e8fd7
Aravindhan V <aravindhan.v@perfios.com>
2021-08-31 16:09:00 +0530

    ADDED: Created Node class

---
+++ src/com/bridgelabz/datastructures/Node.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.datastructures;
+
+public class Node {
+	private int data;
+	private Node next;
+	
+	public Node(int data, Node next) {
+		this.data = data;
+		this.next = next;
+	}
+}
