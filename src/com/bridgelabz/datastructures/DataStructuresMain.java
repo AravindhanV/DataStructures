d119a4a997a23318adf4e929205cf8d9dfb01b08
Aravindhan V <aravindhan.v@perfios.com>
2021-08-31 17:58:46 +0530

    MODIFIED: updated main to insert node after given key

--- src/com/bridgelabz/datastructures/DataStructuresMain.java
+++ src/com/bridgelabz/datastructures/DataStructuresMain.java
@@ -4,8 +4,9 @@ public class DataStructuresMain {
 	public static void main(String[] args) {
 		LinkedList<Integer> linkedList  = new LinkedList();
 		linkedList.append(new Node(56));
-		linkedList.append(new Node(30));
 		linkedList.append(new Node(70));
+		
+		linkedList.add(new Node(30),56);
 		
 		linkedList.printList();
 	}
