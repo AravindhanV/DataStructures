f92c741f55beb84d6226e69768debfeca5f373bb
Aravindhan V <aravindhan.v@perfios.com>
2021-08-31 17:24:27 +0530

    MODIFED: changed main to demonstrate LinkedList add()

--- src/com/bridgelabz/datastructures/DataStructuresMain.java
+++ src/com/bridgelabz/datastructures/DataStructuresMain.java
@@ -2,14 +2,9 @@ package com.bridgelabz.datastructures;
 
 public class DataStructuresMain {
 	public static void main(String[] args) {
-		Node<Integer> first = new Node(56);
-		Node<Integer> second = new Node(30);
-		Node<Integer> third = new Node(70);
-		
-		first.setNext(second);
-		second.setNext(third);
-		
-		boolean result = first.getNext().equals(second) && second.getNext().equals(third);
-		System.out.println(result);
+		LinkedList<Integer> linkedList  = new LinkedList();
+		linkedList.add(new Node(70));
+		linkedList.add(new Node(30));
+		linkedList.add(new Node(56));
 	}
 }
