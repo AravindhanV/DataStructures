3e654b53adc6578195b4feba47263ddba907c42c
Aravindhan V <aravindhan.v@perfios.com>
2021-08-31 20:31:19 +0530

    MODIFIED: updated main to demonstrate stack

--- src/com/bridgelabz/datastructures/DataStructuresMain.java
+++ src/com/bridgelabz/datastructures/DataStructuresMain.java
@@ -2,14 +2,9 @@ package com.bridgelabz.datastructures;
 
 public class DataStructuresMain {
 	public static void main(String[] args) {
-		LinkedList<Integer> linkedList  = new LinkedList();
-		linkedList.append(new Node(56));
-		linkedList.append(new Node(30));
-		linkedList.append(new Node(40));
-		linkedList.append(new Node(70));
-		
-		System.out.println(linkedList.size());
-		linkedList.deleteNode(40);
-		System.out.println(linkedList.size());
+		Stack<Integer> stack = new Stack();
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
 	}
 }
