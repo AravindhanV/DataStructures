b5048ca546d74330a537755caf40cce439bc796d
Aravindhan V <aravindhan.v@perfios.com>
2021-08-31 16:07:26 +0530

    ADDED: LinkedList class created

---
+++ src/com/bridgelabz/datastructures/DataStructuresMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.datastructures;
+
+public class DataStructuresMain {
+	public static void main(String[] args) {
+		LinkedList linkedList = new LinkedList();
+	}
+}
---
+++ src/com/bridgelabz/datastructures/LinkedList.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.datastructures;
+
+public class LinkedList {
+	Node head;
+	Node tail;
+	
+	public LinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+}
