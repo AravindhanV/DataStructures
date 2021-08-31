3f50ea65687f5e6766c09954aec49a2515450680
Aravindhan V <aravindhan.v@perfios.com>
2021-08-31 17:39:41 +0530

    MODIFIED: updated main to demonstrate append() on LinkedList

--- src/com/bridgelabz/datastructures/DataStructuresMain.java
+++ src/com/bridgelabz/datastructures/DataStructuresMain.java
@@ -3,9 +3,9 @@ package com.bridgelabz.datastructures;
 public class DataStructuresMain {
 	public static void main(String[] args) {
 		LinkedList<Integer> linkedList  = new LinkedList();
-		linkedList.add(new Node(70));
-		linkedList.add(new Node(30));
-		linkedList.add(new Node(56));
+		linkedList.append(new Node(56));
+		linkedList.append(new Node(30));
+		linkedList.append(new Node(70));
 		
 		linkedList.printList();
 	}
