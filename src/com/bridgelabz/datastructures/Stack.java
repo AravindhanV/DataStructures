213b953c7312e0e6452d42d77070aaf2550537c7
Aravindhan V <aravindhan.v@perfios.com>
2021-08-31 20:29:22 +0530

    ADDED: push() implemented in Stack class

--- src/com/bridgelabz/datastructures/Stack.java
+++ src/com/bridgelabz/datastructures/Stack.java
@@ -1,9 +1,13 @@
 package com.bridgelabz.datastructures;
 
-public class Stack {
-	LinkedList list;
+public class Stack<T> {
+	LinkedList<T> list;
 	
 	public Stack() {
 		this.list = new LinkedList();
+	}
+	
+	public void push(T data) {
+		list.add(new Node<T>(data));
 	}
 }
