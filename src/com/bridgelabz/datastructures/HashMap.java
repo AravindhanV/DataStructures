77349039aac2554a10ca02248762bd882670b374
Aravindhan V <aravindhan.v@perfios.com>
2021-09-01 14:50:05 +0530

    MODIFIED: HashMap changed to contain multiple linkedLists in buckets

--- src/com/bridgelabz/datastructures/HashMap.java
+++ src/com/bridgelabz/datastructures/HashMap.java
@@ -1,22 +1,46 @@
 package com.bridgelabz.datastructures;
 
+import java.util.*;
+
 public class HashMap<K,V> {
-	LinkedList<K> linkedList;
+	private int numBuckets;
+	ArrayList<LinkedList<K>> bucketArray;
 	
 	public HashMap() {
-		this.linkedList = new LinkedList();
+		this.numBuckets = 10;
+		this.bucketArray = new ArrayList<>(numBuckets);
+		for(int i=0;i<numBuckets;i++) {
+			bucketArray.add(null);
+		}
+	}
+	
+	private int getBucketIndex(K key) {
+		int hashCode = Math.abs(key.hashCode());
+		int index = hashCode % numBuckets;
+		return index;
 	}
 	
 	public V get (K key) {
+		int index = this.getBucketIndex(key);
+		LinkedList<K> linkedList = bucketArray.get(index);
+		if(linkedList == null) {
+			return null;
+		}
 		MapNode<K,V> mapNode = (MapNode<K,V>) linkedList.search(key);
 		return (mapNode == null) ? null : mapNode.getValue();
 	}
 	
 	public void add(K key, V value) {
+		int index = this.getBucketIndex(key);
+		LinkedList<K> linkedList = bucketArray.get(index);
+		if(linkedList == null) {
+			linkedList = new LinkedList();
+			bucketArray.set(index, linkedList);
+		}
 		MapNode<K,V> mapNode = (MapNode<K,V>) linkedList.search(key);
 		if(mapNode == null) {
 			mapNode = new MapNode(key,value);
-			this.linkedList.append(mapNode);
+			linkedList.append(mapNode);
 		} else {
 			mapNode.setValue(value);
 		}
